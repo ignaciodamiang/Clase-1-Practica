f33d0acecc9f5cb5a7242badb8f9b699adcdf3b3
Rodrigo <71408813+rvillegas505@users.noreply.github.com>
2022-04-06 19:46:24 -0300

    Agregando juego BolaOcho Consola

---
+++ PW3-2022-1C-Clase-1.Entidades/BolaOcho.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW3_2022_1C_Clase_1.Entidades
+{
+    public static class BolaOcho
+    {
+        public static string[] arrayFrases = new string[] { "It is certain", "Yes definitely", "Signs point to yes.",
+            "Reply hazy, try again.", "Ask again later.", "Better not tell you now.",
+            "Don't count on it.","My reply is no.", "Very doubtful." };
+
+
+        public static string DarFrase()
+        {
+            int cantidadFrasesEnArray = arrayFrases.Length;
+            
+            Random rand = new Random();
+            int numeroFrase = rand.Next(0, cantidadFrasesEnArray);
+
+            string frase = arrayFrases[numeroFrase];
+            return frase;
+        }
+
+
+    }
+}
